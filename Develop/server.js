3faecc1324979e03e6d3e7208a833a094ab2bfc6
Drew Kempen <drewkempen98@gmail.com>
2022-06-03 23:18:11 -0400

    Fix port issue

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
